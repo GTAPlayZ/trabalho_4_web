52275a89f1c213cb0935fe3670a0226b3da27433
trabalho <92698625+GTAPlayZ@users.noreply.github.com>
2021-11-03 22:17:44 -0300

    files;

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# trabalho_4_web
+# trabalho_4_programacao_web
+
+# Nome da empresa
+    Pensamos em Sion e como tratamos com solução adicionamos o Solutions, ficando Sion Solutions
+
+# Logo marca
+![Sion Solutions](/assets/images/logo_marca_final.png "Logo marca")
+# Tipografia
+    Poppins
+    DM Sans
+# Cores
+[Paleta de Cores](https://coolors.co/30b3de-305cde-5b30de)
+    #30b3de
+    #305cde
+    #5b30de
+# Icones
+[IcoMoon](https://icomoon.io/)
+# Imagens
+[Pixabay](https://pixabay.com/pt/)
---
+++ assets/js/index.js
@@ -0,0 +1,87 @@
+
+window.onload = function () {
+
+  // ativa os eventos de click no menu
+  const nav = document.querySelector("#header nav");
+  const toggle = document.querySelectorAll("nav .toggle");
+  for (const element of toggle) {
+    element.addEventListener("click", function () {
+      nav.classList.toggle("show");
+    });
+  }
+
+  const voltarParaTopoButton = document.querySelector(".back-to-top");
+  const sections = document.querySelectorAll("main section[id]");
+
+  // ativa o swiper
+  const swiper = new Swiper(".swiper-container", {
+    slidesPerView: 1,
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+    breakpoints: {
+      767: {
+        slidesPerView: 2,
+        setWrapperSize: true,
+      },
+    },
+  });
+
+  // ativa o ScrollReveal
+  const scrollReveal = ScrollReveal({
+    origin: "top",
+    distance: "30px",
+    duration: 700,
+    reset: true,
+  });
+  scrollReveal.reveal(
+    `#inicio .image, #inicio .text,
+  #sobre .image, #sobre .text,
+  #servicos header, #servicos .card,
+  #portfolio header, #portfolio .portfolio
+  #contato .text, #contato .links,
+  footer .brand, footer .social
+  `,
+    { interval: 100 }
+  );
+
+  // tratamentos no scroll da landing page
+  window.addEventListener("scroll", function () {
+    voltarParaTopo(voltarParaTopoButton);
+    ativarMenuDuranteScroll(sections);
+  });
+
+};
+
+function voltarParaTopo(voltarParaTopoButton) {
+  if (window.scrollY >= 560) {
+    voltarParaTopoButton.classList.add("show");
+  } else {
+    voltarParaTopoButton.classList.remove("show");
+  }
+}
+
+function ativarMenuDuranteScroll(sections) {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4;
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    const sectionId = section.getAttribute("id");
+
+    const checkpointStart = checkpoint >= sectionTop;
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight;
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector("nav ul li a[href*=" + sectionId + "]")
+        .classList.add("active");
+    } else {
+      document
+        .querySelector("nav ul li a[href*=" + sectionId + "]")
+        .classList.remove("active");
+    }
+  }
+}
